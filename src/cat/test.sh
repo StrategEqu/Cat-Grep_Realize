85b2d6fd37684f18a331f3a9a9ef2d4227109466
Strategy <Dima.kogtev.95@mail.ru>
2024-01-08 01:56:25 +0300

    last fix

--- src/cat/test.sh
+++ src/cat/test.sh
@@ -1,42 +1,39 @@
+#!/bin/bash
 SUCCESS='\e[32m'
 RESET='\033[0m'
 FAILURE='\033[0;31m'
 
 file="test2.txt"
 compile="s21_cat"
-flags=("-e" "-s" "-T" "-E" "-b" "-n" "-t" "--number" 
-"--number-nonblank" "--squeeze-blank" "--show-ends" "--show-tabs")
+flags=("-e" "-s" "-T" "-E" "-b" "-n" "-t" "--number"
+	"--number-nonblank" "--squeeze-blank" "--show-ends" "--show-tabs")
 
-    # Перебираем флаги
-    for flag in "${flags[@]}"
-    do
-            # Проверяем наличие скомпилированного файла
-        if [[ ! -f "$compile" ]]; then 
-            echo "Файл $compile не существует."
-            exit 1
-        fi
-            # Проверяем наличие файлов и удаляем временные файлы, если false
-        if [[ ! -f "$file" ]]; then
-            echo "Файл $file не существует."
-            rm output_$flag_$file.txt output2_$flag_$file.txt
-            exit 1
-        fi    
+# Перебираем флаги
+for flag in "${flags[@]}"; do
+	# Проверяем наличие скомпилированного файла
+	if [[ ! -f "$compile" ]]; then
+		echo "Файл $compile не существует."
+		exit 1
+	fi
+	# Проверяем наличие файлов и удаляем временные файлы, если false
+	if [[ ! -f "$file" ]]; then
+		echo "Файл $file не существует."
+		rm "output_${flag}_${file}.txt" "output2_${flag}_${file}.txt"
+		exit 1
+	fi
 
-        # Запускаем программу с флагами и файлами и сохраняем вывод в файл
-        ./s21_cat "$flag" "$file" > "output_$flag_$file.txt"
-        # Запускаем команду cat с флагами и файлами и сохраняем вывод в файл
-        cat "$flag" "$file" > "output2_$flag_$file.txt"
+	# Запускаем программу с флагами и файлами и сохраняем вывод в файл
+	./s21_cat "$flag" "$file" >"output_${flag}_${file}.txt"
+	# Запускаем команду cat с флагами и файлами и сохраняем вывод в файл
+	cat "$flag" "$file" >"output2_${flag}_${file}.txt"
 
-        # Сравниваем вывод программы с ожидаемым выводом
-        diff "output_$flag_$file.txt" "output2_$flag_$file.txt"
+	# Сравниваем вывод программы с ожидаемым выводом
+	if diff "output_${flag}_${file}.txt" "output2_${flag}_${file}.txt"; then
+		echo -e "${SUCCESS}[SUCCESS]${RESET} Команды идентичны для флага $flag и файла $file."
+	else
+		echo -e "${FAILURE}[FAIL]${RESET} Команды отличаются для флага $flag и файла $file."
+	fi
 
-        # Проверяем код возврата команды diff
-        if [[ $? -eq 0 ]]; then
-            echo -e "${SUCCESS}[SUCCESS]${RESET} Команды идентичны для флага $flag и файла $file."
-        else
-            echo -e "${FAILURE}[FAIL]${RESET} Команды отличаются для флага $flag и файла $file."
-        fi
-
-        # Удаляем временный файл
-        rm output_$flag_$file.txt output2_$flag_$file.txt
-    done
+	# Удаляем временные файлы
+	rm "output_${flag}_${file}.txt" "output2_${flag}_${file}.txt"
+done
--- src/grep/test.sh
+++ src/grep/test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 SUCCESS='\e[32m'
 RESET='\033[0m'
 FAILURE='\033[0;31m'
@@ -7,36 +8,53 @@ pattern="pattern.txt"
 compile="s21_grep"
 flags=("-v" "-i" "-o" "-l" "-n" "-c" "-e" "-f" "-s" "-h")
 
-    # Перебираем флаги
-    for flag in "${flags[@]}"
-    do
-            # Проверяем наличие скомпилированного файла
-        if [[ ! -f "$compile" ]]; then 
-            echo "Файл $compile не существует."
-            exit 1
-        fi
-            # Проверяем наличие файлов и удаляем временные файлы, если false
-        if [[ ! -f "$file" ]]; then
-            echo "Файл $file не существует."
-            rm output_$flag_$file.txt output2_$flag_$file.txt
-            exit 1
-        fi    
+# Перебираем флаги
+for flag in "${flags[@]}"; do
+	# Проверяем наличие скомпилированного файла
+	if [[ ! -f "$compile" ]]; then
+		echo "Файл $compile не существует."
+		exit 1
+	fi
+	# Проверяем наличие файлов и удаляем временные файлы, если false
+	if [[ ! -f "$file" ]]; then
+		echo "Файл $file не существует."
+		rm "output_${flag}_${file}.txt" "output2_${flag}_${file}.txt"
+		exit 1
+	fi
 
-        # Запускаем программу с флагами и файлами и сохраняем вывод в файл
-        ./s21_grep "$flag" "$pattern" "$file" > "output_$flag_$file.txt"
-        # Запускаем команду cat с флагами и файлами и сохраняем вывод в файл
-        grep "$flag" "$pattern" "$file" > "output2_$flag_$file.txt"
+	# Запускаем программу с флагами и файлами и сохраняем вывод в файл
+	./s21_grep "$flag" "$pattern" "$file" >"output_${flag}_${file}.txt"
+	# Запускаем команду grep с флагами и файлами и сохраняем вывод в файл
+	grep "$flag" "$pattern" "$file" >"output2_${flag}_${file}.txt"
 
-        # Сравниваем вывод программы с ожидаемым выводом
-        diff "output_$flag_$file.txt" "output2_$flag_$file.txt"
+	# Сравниваем вывод программы с ожидаемым выводом
+	if diff "output_${flag}_${file}.txt" "output2_${flag}_${file}.txt"; then
+		echo -e "${SUCCESS}[SUCCESS]${RESET} Команды идентичны для флага $flag и файла $file."
+	else
+		echo -e "${FAILURE}[FAIL]${RESET} Команды отличаются для флага $flag и файла $file."
+	fi
 
-        # Проверяем код возврата команды diff
-        if [[ $? -eq 0 ]]; then
-            echo -e "${SUCCESS}[SUCCESS]${RESET} Команды идентичны для флага $flag и файла $file."
-        else
-            echo -e "${FAILURE}[FAIL]${RESET} Команды отличаются для флага $flag и файла $file."
-        fi
+	# Удаляем временные файлы
+	rm "output_${flag}_${file}.txt" "output2_${flag}_${file}.txt"
+done
 
-        # Удаляем временный файл
-        rm output_$flag_$file.txt output2_$flag_$file.txt
-    done
+# Перебираем первый флаг
+for ((i = 0; i < ${#flags[@]}; i++)); do
+	flag1=${flags[i]}
+
+	# Перебираем второй флаг
+	for ((j = i + 1; j < ${#flags[@]}; j++)); do
+		flag2=${flags[j]}
+
+		./s21_grep "$flag1" "$pattern" "$file" >"output_${flag1}_${flag2}_${file}.txt"
+		grep "$flag1" "$pattern" "$file" >"output2_${flag1}_${flag2}_${file}.txt"
+
+		if diff "output_${flag1}_${flag2}_${file}.txt" "output2_${flag1}_${flag2}_${file}.txt"; then
+			echo -e "${SUCCESS}[SUCCESS]${RESET} Команды идентичны для флагов $flag1 и $flag2 и файла $file."
+		else
+			echo -e "${FAILURE}[FAIL]${RESET} Команды отличаются для флагов $flag1 и $flag2 и файла $file."
+		fi
+
+		rm "output_${flag1}_${flag2}_${file}.txt" "output2_${flag1}_${flag2}_${file}.txt"
+	done
+done
